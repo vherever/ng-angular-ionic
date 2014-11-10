b04ab8f8512327bf021010c11bdd7a15c266a421
Vladimir Vherever <vherever@gmail.com>
2014-11-10 23:42:39 +0200

    finally deployed to firebase

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ var express = require('express'),
 	.get('*', function(req, res) {
 		res.sendfile('index.html');
 	})
-	.listen(3000);
-console.log('Listening on port 3000...');
+	.listen(process.env.PORT || 3000, function() {
+		console.log('Listening on port 3000...');
+	});
